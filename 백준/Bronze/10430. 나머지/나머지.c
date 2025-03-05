fecf6f19fd9f8487ca730ac0ed0a2353e3cd8967
윤승준 <118863151+ddoonkorea@users.noreply.github.com>
2025-03-05 16:52:53 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.c
@@ -0,0 +1,18 @@
+//hello.c
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    int A,B,C;
+
+    scanf("%d %d %d", &A, &B, &C);
+    printf("%d\n", (A+B)%C);
+    printf("%d\n", (((A%C) + (B%C))%C));
+    printf("%d\n", (A*B)%C);
+    printf("%d\n", ((A%C) * (B%C))%C);
+
+    return 0;
+}
+
