ba2620061b0157e7db3020aa8a6d2f1c8f9c6df4
윤승준 <118863151+ddoonkorea@users.noreply.github.com>
2025-03-05 17:50:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,16 @@
+//hello.c
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    long long a, b, c;
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+    
+    printf("%lld", a+b+c);
+
+    return 0;
+}
+
