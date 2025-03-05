87b76774e49a5452990058a5768fd053ef0967ce
윤승준 <118863151+ddoonkorea@users.noreply.github.com>
2025-03-05 16:31:30 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,17 @@
+//hello.c
+
+#include <stdio.h>
+
+int main(void) {
+    int a,b;
+
+    scanf("%d %d", &a,&b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+
+    return 0;
+}
+
