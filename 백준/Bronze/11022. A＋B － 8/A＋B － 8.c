95fcf7dd44be4c0832096ca1bba0faddca5bb6b5
윤승준 <118863151+ddoonkorea@users.noreply.github.com>
2025-03-07 16:25:32 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,18 @@
+//hello.c
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    int num;
+    scanf("%d", &num);
+    for (int i = 1; i<= num; i++) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a+b);
+    }
+
+   return 0;
+}
+ 
