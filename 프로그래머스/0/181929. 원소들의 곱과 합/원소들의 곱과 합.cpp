f02c976c3574a335bcb261be733ba7a906e92018
윤승준 <118863151+ddoonkorea@users.noreply.github.com>
2025-06-18 22:12:19 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -7,17 +7,11 @@ int solution(vector<int> num_list) {
     int answer = 0;
     int sum = 1;
     int mul = 0;
-    int dob = 0;
     
     for (int i = 0; i<num_list.size(); i++) {
         sum *= num_list[i];
         mul += num_list[i];
     }
-    dob = mul * mul;
     
-    if (sum < dob) {
-        answer = 1;
-    } else answer = 0;
-
-    return answer;
+    return (sum < mul * mul) ? 1 : 0;
 }
